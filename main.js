1e2d9d6116e78a777a4496cd9af7070f57fc037d
Lyndonna Munro <lyndonnamunro@Lyndonnas-Air.verizon.net>
2022-03-07 09:34:46 -0500

    Created SSH Key

--- main.js
+++ main.js
@@ -2,24 +2,45 @@ const feedEl= document.getElementById("fButton");
 const sleepEl= document.getElementById("sButton");
 const playEl= document.getElementById("pButton");
 
+
 const hunEl= document.getElementById("hun");
 const borEl= document.getElementById("bor");
 const sleEl= document.getElementById("sle");
 const ageEl= document.getElementById("age");
 const staEl= document.getElementById("sta");
 
-function feedClick(){
-    console.log("Feed button clicked")
+let pet_stats ={
+    "Hunger" : 0,
+    "Boredom" : 0,
+    "Sleep" : 0,
+    "Age" : 0,
+    "Alive" : true,
+}
+
+
+
+function feedClicked(){
+    let food = 6;
+    if(food>=10){
+        console.log("Your pet died! You let your pet play too much!")
+    }   
 };
 
-function sleepClick(){
-    console.log("sleep button clicked")
+function sleepClicked(){
+    let sleep =6;
+    if(sleep>=10){
+        console.log("Your pet died! You let your pet sleep too much!")
+    }   
+
 };
 
-function playClick(){
-    console.log("play button clicked")
+function playClicked(){
+    let play =6;
+    if(play>=10){
+        console.log("Your pet died! You let your pet play too much!")
+    }   
 };
 
-feedEl.addEventListener('click', feedClick);
-sleepEl.addEventListener('click', sleepClick);
-playEl.addEventListener('click', playClick);
+feedEl.addEventListener('click', feedClicked);
+sleepEl.addEventListener('click', sleepClicked);
+playEl.addEventListener('click', playClicked);
