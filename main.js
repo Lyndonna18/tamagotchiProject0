707196c363df3eb0db1416d19c9a700453fb64b1
Lyndonna Munro <lyndonnamunro@Lyndonnas-Air.verizon.net>
2022-03-07 07:05:21 -0500

    More changes

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-console.log("Is this working?");
+const feedEl= document.getElementById("fButton");
+const sleepEl= document.getElementById("sButton");
+const playEl= document.getElementById("pButton");
 
-addEventListener
+const hunEl= document.getElementById("hun");
+const borEl= document.getElementById("bor");
+const sleEl= document.getElementById("sle");
+const ageEl= document.getElementById("age");
+const staEl= document.getElementById("sta");
+
+function feedClick(){
+    console.log("Feed button clicked")
+};
+
+function sleepClick(){
+    console.log("sleep button clicked")
+};
+
+function playClick(){
+    console.log("play button clicked")
+};
+
+feedEl.addEventListener('click', feedClick);
+sleepEl.addEventListener('click', sleepClick);
+playEl.addEventListener('click', playClick);
